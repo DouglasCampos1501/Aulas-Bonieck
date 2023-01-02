8397ed97c36305617086b1867d3bb5acc5e2fd3c
Douglas Campos <dougcampos1501@gmail.com>
2023-01-02 17:10:12 -0300

    Exercício JS - B7W

---
+++ metodoString.js
@@ -0,0 +1,8 @@
+let nome = "Douglas Anderson de Campos"
+// console.log(nome.length);
+
+//let resultado = nome.length;
+//console.log(resultado);
+
+let resultado = nome.indexOf("Campos");
+console.log(resultado);
