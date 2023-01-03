d091a7f87bb1a179a96d015d82877627c02086fb
Douglas Campos <dougcampos1501@gmail.com>
2023-01-03 10:45:03 -0300

    Maniopulação String

--- metodoString.js
+++ metodoString.js
@@ -4,5 +4,23 @@ let nome = "Douglas Anderson de Campos"
 //let resultado = nome.length;
 //console.log(resultado);
 
-let resultado = nome.indexOf("Campos");
+//let resultado = nome.indexOf("Campos");
+//console.log(resultado);
+
+//let result = nome.slice(5, 10);
+//let result = nome.slice(-10);
+//let result = nome.substring(5, 10);
+//let result = nome.substr(5, 10);
+let result = nome.substr(-4, 10);
+console.log(result);
+
+let nome2 = 'Fulano da silva sousa';
+let busca = nome2.slice(-5);
+console.log(busca)
+
+//let resultado = nome.replace('Campos', 'Souza');
+//let resultado = nome.toUpperCase();
+let resultado = nome.toLowerCase();
 console.log(resultado);
+
+
